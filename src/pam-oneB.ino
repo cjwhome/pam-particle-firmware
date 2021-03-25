a5ea39c2fdcfe521f5ec6b41675784ba842c2224
craig williford <craigjwilliford@gmail.com>
2021-03-25 11:58:46 -0600

    disabled CO2 measurements for testing out PAM-1043

--- src/pam-oneB.ino
+++ src/pam-oneB.ino
@@ -37,7 +37,7 @@
 GoogleMapsDeviceLocator locator;
 
 #define APP_VERSION 7
-#define BUILD_VERSION 91
+#define BUILD_VERSION 92
 
 //define constants
 #define SEALEVELPRESSURE_HPA (1013.25)
@@ -568,7 +568,7 @@ void readStoredVars(void){
     EEPROM.get(BATTERY_THRESHOLD_ENABLE_MEM_ADDRESS, battery_threshold_enable);
     EEPROM.get(ABC_ENABLE_MEM_ADDRESS, abc_logic_enabled);
     EEPROM.get(HIH8120_ENABLE_MEM_ADDRESS, hih8120_enabled);
-    hih8120_enabled = 0;
+    //hih8120_enabled = 0;
     EEPROM.get(CO_SOCKET_MEM_ADDRESS, CO_socket);
     EEPROM.get(GOOGLE_LOCATION_MEM_ADDRESS, google_location_en);
     EEPROM.get(SENSIBLEIOT_ENABLE_MEM_ADDRESS, sensible_iot_en);
@@ -949,13 +949,13 @@ void setup()
         writeLogFile("Initialized BME Sensor");
     }
 
-    if(!t6713.begin()){
+    /*if(!t6713.begin()){
       Serial.println("Could not find a valid T6713 sensor, check wiring!");
       if(debugging_enabled)
           writeLogFile("Could not find a valid T6713");
     }else{
         Serial.println("Initilized T6713")
-    }
+    }*/
   //Serial.println("before bme setup");
     // Set up oversampling and filter initialization
     bme.setTemperatureOversampling(BME680_OS_8X);
@@ -1066,7 +1066,7 @@ void loop() {
     //CO_float_2 += CO_zero_2;
     //CO_float_2 *= CO_slope_2;
 
-    CO2_float = readCO2();
+    //CO2_float = readCO2();
 
 
     //correct for altitude
