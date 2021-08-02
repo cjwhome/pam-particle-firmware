0b457e993d46c83e02879376da97def30c54af3b
austinbailly441 <austinbailly@gmail.com>
2021-08-02 11:44:17 -0600

    messing around with functions

--- src/pam-oneB.ino
+++ src/pam-oneB.ino
@@ -1,19 +1,3 @@
-/***************************************************************************
-  This is a library for the BME680 gas, humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME680 Breakout
-  ----> http://www.adafruit.com/products/3660
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
 #include <string>
 //#include <Wire.h>
 //#include <SPI.h>
@@ -713,7 +697,7 @@ void setup()
     digitalWrite(fiveVolt_en, HIGH);
 
     // register the cloud function
-    Particle.function("rebootaqsync", rebootAQSync);
+    Particle.function("RebootADevice", rebootDevice);
     Particle.function("diagnostics", sendDiagnostics);
     //Particle.variable("CO_zeroA", CO_zeroA);
     //debugging_enabled = 1;  //for testing...
@@ -2183,8 +2167,9 @@ void getEspAQSyncData(char incomingByte)
     }
 }
 
-int rebootAQSync(String nothing)
+int rebootAQSync(String deviceName)
 {
+    String command = "R PowerRelay|R " + deviceName; 
     serBuf.write('f R');
     return 1;
 }
