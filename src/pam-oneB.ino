59a797931832e5bee683af33121782f06c6f1802
austinbailly441 <austinbailly@gmail.com>
2021-08-12 15:23:34 -0600

    started on adding change settings to aqsync

--- src/pam-oneB.ino
+++ src/pam-oneB.ino
@@ -704,6 +704,7 @@ void setup()
     // register the cloud function
     Particle.function("RebootADevice", rebootDevice);
     Particle.function("diagnostics", sendDiagnostics);
+    Particle.function("setSetting", setSetting);
     //Particle.variable("CO_zeroA", CO_zeroA);
     //debugging_enabled = 1;  //for testing...
     //initialize serial1 for communication with BLE nano from redbear labs
@@ -2113,6 +2114,24 @@ int sendDiagnostics(String deviceName)
     }
     return 0;
 
+}
+
+int setSetting(String information)
+{
+    String deviceName = information.substring(0, information.indexOf(','));
+    information = information.substring(information.indexOf(',')+1, information.length());
+    String settingMarker = information.substring(0, information.indexOf(','));
+    information = information.substring(information.indexOf(',')+1, information.length());
+    String value = information.substring(0, information.indexOf(','));
+    if (deviceName == "" || settingMarker == "" || value == "")
+    {
+        Serial.println("The right information was not sent over cellular. Try again");
+        return 0;
+    }
+    String command = "S,"+deviceName+","+settingMarker+","+value;
+    command += checksumMaker(command)+'\n';
+    serBuf.print(command);
+    return 1;
 }
 
 void readHIH8120(void)
